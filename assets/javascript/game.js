dea81a0fd134292824ee2c22424e78538474337b
doncasanova <ethanjuan@yahoo.com>
2018-03-29 22:21:22 -0500

    added picture update and added a for loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,27 @@ var things = ['harmonica', 'guitar', 'banjo', 'mandolin', 'piano', 'drum'];
 var thing = things[Math.floor(Math.random() * things.length)];
 console.log(thing)
 var used_characters = [];
-var tries_remaining = 5
+var tries_remaining = 10
 var correct_letter = 0
+var picture = 1
+//--------------------------------------------------------------
+function update_try(used_characters, tries_remaining, n, thing, init) {
+    //used charactor list
+    var length = used_characters.length;
+    document.getElementById("used-characters").innerHTML = used_characters;
+    console.log(used_characters)
 
+    //tallies the missed count
+    console.log(tries_remaining);
+    document.getElementById("tries-remaining").innerHTML = "Tries remaining    " + tries_remaining;
+
+    //changes the instructions to good luck
+    console.log(tries_remaining);
+    document.getElementById("insturctions").innerHTML = "Good luck here you go!    ";
+
+    document.getElementById("character-length").innerHTML = "Your word has    " + n + "  letters";
+    document.getElementById("character-no").innerHTML = "Word to find      " + thing + init;
+}
 //---------------------------------------------------------------
 //number of characters
 var n = thing.length;
@@ -21,7 +39,8 @@ for (var i = 0; i < n; i++) {
     console.log(spaces)
 
     document.getElementById("character-spaces").innerHTML = spaces;
-
+    //-------------------------------------------------------------
+    update_try(used_characters, tries_remaining, n, thing, "Game initialized")
     //--------------------------------------------------------------
     //key stroke events
     document.onkeyup = function (event) {
@@ -34,12 +53,14 @@ for (var i = 0; i < n; i++) {
                 console.log(correct_letter + "   correct letter count")
             }
         }
+for (var i = 0; i < thing.length; i++){
+
 
         //---------------------------------------------------------------
-        var first_character = thing.charAt(0);
+        var first_character = thing.charAt(i);
         console.log(first_character)
         if (letter === first_character) {
-            spaces[0] = letter;
+            spaces[i] = letter;
             n--
             // console.log("this is it" + n)
             // if (n <= 0){
@@ -47,96 +68,9 @@ for (var i = 0; i < n; i++) {
             // }
         }
         document.getElementById("character-spaces").innerHTML = spaces;
-        var second_character = thing.charAt(1);
-        console.log(second_character)
-        if (letter === second_character) {
-            spaces[1] = letter;
-            n--
-            // if (n <= 0){
-            //     alert("you won!!!")
-            // }
-        }
-        document.getElementById("character-spaces").innerHTML = spaces;
-        var third_character = thing.charAt(2);
-        console.log(third_character)
-        if (letter === third_character) {
-            spaces[2] = letter;
-            n--
-            // if (n <= 0){
-            //     alert("you won!!!")
-            // }
-        }
-        document.getElementById("character-spaces").innerHTML = spaces;
-        var forth_character = thing.charAt(3);
-        console.log(forth_character)
-        if (letter === forth_character) {
-            spaces[3] = letter;
-            n--
-            // if (n <= 0){
-            //     alert("you won!!!")
-            // }
-        }
-        document.getElementById("character-spaces").innerHTML = spaces;
-        var fifth_character = thing.charAt(4);
-        console.log(fifth_character)
-        if (letter === fifth_character) {
-            spaces[4] = letter;
-            n--
-            // if (n <= 0){
-            //     alert("you won!!!")
-            // }
-        }
-        document.getElementById("character-spaces").innerHTML = spaces;
-        var sixth_character = thing.charAt(5);
-        console.log(sixth_character)
-        if (letter === sixth_character) {
-            spaces[5] = letter;
-            n--
-            // if (n <= 0){
-            //     alert("you won!!!")
-            // }
-        }
-        document.getElementById("character-spaces").innerHTML = spaces;
-        var seventh_character = thing.charAt(6);
-        console.log(seventh_character)
-        if (letter === seventh_character) {
-            spaces[6] = letter;
-            n--
-            // if (n <= 0){
-            //     alert("you won!!!")
-            // }
-        }
-        document.getElementById("character-spaces").innerHTML = spaces;
-        var eight_character = thing.charAt(7);
-        console.log(eight_character)
-        if (letter === eight_character) {
-            spaces[7] = letter;
-            n--
-            // if (n <= 0){
-            //     alert("you won!!!")
-            // }
-        }
-        document.getElementById("character-spaces").innerHTML = spaces;
-        var nine_character = thing.charAt(7);
-        console.log(nine_character)
-        if (letter === nine_character) {
-            spaces[7] = letter;
-            n--
-            // if (n <= 0){
-            //     alert("you won!!!")
-            // }
-        }
-        document.getElementById("character-spaces").innerHTML = spaces;
-        var ten_character = thing.charAt(7);
-        console.log(ten_character)
-        if (letter === ten_character) {
-            spaces[7] = letter;
-            n--
-            // if (n <= 0){
-            //     alert("you won!!!")
-            // }
-        }
-        document.getElementById("character-spaces").innerHTML = spaces;
+    }
+       
+
         console.log(spaces)
 
         //player wins
@@ -151,6 +85,18 @@ for (var i = 0; i < n; i++) {
         //open araay to store used characters
 
 
+        // $('.game--backdrop').css('background', 'url(hangman2.jpg) no-repeat');
+        // $('.game--backdrop').css('background-size', '100% 100%');
+
+
+
+        // function SwitchPic(pic) {
+        //     var image = document.getElementsByClassName("game--backdrop");
+        //      {
+        //         image.src = "hangman2.jpg";
+        //     } 
+        // }
+
 
 
         if (used_characters.includes(letter) == false) {
@@ -158,6 +104,9 @@ for (var i = 0; i < n; i++) {
             //tally count down from max number of tries
             if (thing.indexOf(letter) === -1) {
                 tries_remaining--;
+                picture++
+                console.log(picture + "picture count")
+                $('.game--backdrop').removeClass("game--backdrop1").addClass('game--backdrop' + picture);
                 if (tries_remaining === 0) {
 
                     alert("Your Dead!! Press ok to start again.")
@@ -167,21 +116,7 @@ for (var i = 0; i < n; i++) {
             }
         }
 
-        //used charactor list
-        var length = used_characters.length;
-        document.getElementById("used-characters").innerHTML = used_characters;
-        console.log(used_characters)
-
-        //tallies the missed count
-        console.log(tries_remaining);
-        document.getElementById("tries-remaining").innerHTML = "Tries remaining    " + tries_remaining;
-
-        //changes the instructions to good luck
-        console.log(tries_remaining);
-        document.getElementById("insturctions").innerHTML = "Good luck here you go!    ";
-
-        document.getElementById("character-length").innerHTML = "Your word has    " + n + "  letters";
-        document.getElementById("character-no").innerHTML = "Word to find     " + thing;
+        update_try(used_characters, tries_remaining, n, thing, "End of P")
     }
 
 
