8bf7d6e41ef8c5102866bb1fab43b87a979c08ad
doncasanova <ethanjuan@yahoo.com>
2018-04-05 23:24:39 -0500

    added timers for end of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,14 +14,14 @@ function update_try(used_characters, tries_remaining, n, thing, init) {
     //used charactor list
     var length = used_characters.length;
     document.getElementById("used-characters").innerHTML = used_characters;
-    console.log(used_characters)
+
 
     //tallies the missed count
-    console.log(tries_remaining);
+
     document.getElementById("tries-remaining").innerHTML = "Tries remaining    " + tries_remaining;
 
     //changes the instructions to good luck
-    console.log(tries_remaining);
+
     document.getElementById("insturctions").innerHTML = "Good luck here you go!    ";
 
     document.getElementById("character-length").innerHTML = "Your word has    " + word_length + "  letters";
@@ -31,19 +31,19 @@ function update_try(used_characters, tries_remaining, n, thing, init) {
 //number of characters
 var n = thing.length;
 var word_length = n
-console.log(n + "  letters in word")
+
 
 //--------------------------------------------------------------
 //create spaces
 var spaces = [];
 for (var i = 0; i < thing.length; i++) {
     spaces[i] = "_";
-    console.log(spaces + "  array spaces for word")
+
 
     //----------------------------------------------------------
     //changes the html to show word spaces
     document.getElementById("character-spaces").innerHTML = spaces;
-    console.log(n)
+
 
 
     //----------------------------------------------------------
@@ -58,7 +58,7 @@ for (var i = 0; i < thing.length; i++) {
 
             if (thing[j] === letter) {
                 correct_letter++;
-                console.log(correct_letter + "   correct letter count")
+
             }
         }
         for (var i = 0; i < thing.length; i++) {
@@ -66,23 +66,23 @@ for (var i = 0; i < thing.length; i++) {
 
             //----------------------------------------------------
             var first_character = thing.charAt(i);
-            console.log(first_character)
+
             if (letter === first_character) {
                 spaces[i] = letter;
                 n--
-                console.log("correct letter count  " + n)
+
 
 
             }
             document.getElementById("character-spaces").innerHTML = spaces.join();
-            console.log(n)
+
             if (letter === first_character) {
 
                 if (n <= 0) {
-                    console.log(spaces)
                     setTimeout(function () {
                         alert("you won!!!");
                     }, 50);
+
 
                 }
 
@@ -91,17 +91,17 @@ for (var i = 0; i < thing.length; i++) {
         }
 
 
-        console.log(spaces)
 
         //---------------------------------------------------------------
         //open araay to store used characters
         if (picture === 10) {
             tries_remaining--
             setTimeout(function () {
+                alert("Your Dead!! Press ok to start again.")
             }, 50);
-            alert("Your Dead!! Press ok to start again.")
 
-            window.location.reload()
+
+            // window.location.reload()
         }
 
 
@@ -111,7 +111,7 @@ for (var i = 0; i < thing.length; i++) {
             if (thing.indexOf(letter) === -1) {
                 tries_remaining--;
                 picture++
-                console.log(picture + "  picture count")
+
                 $('.game--backdrop').removeClass("game--backdrop1").addClass('game--backdrop' + picture);
 
 
